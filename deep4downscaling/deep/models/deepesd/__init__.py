401f78d3b5896667fddf546cc4d1d16e4cd2c415
jgonzalezab <jose.gonz.abad@gmail.com>
2026-03-30 14:15:54 +0200

    minor fixes: standardized DeepESD (still adding support for old API) and moved beta from forward to __ini__t for the CRPS loss function

--- deep4downscaling/deep/models/deepesd/__init__.py
+++ deep4downscaling/deep/models/deepesd/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 
-from .deepesd import DeepESDtas, DeepESDpr
+from .deepesd import DeepESD, DeepESDtas, DeepESDpr
 from .noisy import NoisyDeepESD
 from .discriminator import DeepESD_Discriminator
 
